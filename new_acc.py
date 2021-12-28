abd30db1d1c989b9641e288735f98b5ae587c1ae
Khamitz <90205644+Khamitz@users.noreply.github.com>
2021-12-28 22:25:31 +0600

    Update new_acc.py

--- new_acc.py
+++ new_acc.py
@@ -1,7 +1,6 @@
 import cx_Oracle
 import pandas as pd
 from matplotlib import pyplot as plt
-from matplotlib import style
 
 conn = None
 
@@ -24,18 +23,20 @@ def new_acc():
 
     try:
         val = (new_acc2, new_cur2, 0)
-        sql = "insert into bnk values(to_number(:1),:2, to_number(:3))"
+        sql = "insert into bnk values(to_number(:1),:2, to_number(:3))"   #bnk
+        tr2 = "insert into tr2 values(to_number(:1),:2, to_number(:3))"   #таблица с историей изменения баланса
         cursor.execute(sql, tuple(val))
+        cursor.execute(tr2, tuple(val))
         conn.commit()
         print("Аккаунт создан.", val)
     except Exception:
         print("Tакой аккаунт существует.")
 
 
-def transactions():
+def transactions():   #ВСЕ ТРАНЗАКЦИИ ПО СЧЕТУ
     print("Вы выбрали ВСЕ ТРАНЗАКЦИИ ПО СЧЕТУ:")
     id_acc_tr = int(input("Пожалуйста, введите аккаунт айди чтобы увидеть все транзакции по нему:"))
-    query_acc_tr = "SELECT * FROM tr3 where idd={}".format(id_acc_tr)
+    query_acc_tr = "SELECT * FROM tr3 where idd={}".format(id_acc_tr)    # tr3   транзакция каждого счета
     otchet_acc_tr = pd.read_sql(query_acc_tr, con=conn)
     print(otchet_acc_tr)
 
@@ -46,7 +47,6 @@ def transfer():     #перовод
     A = int(input("Пожалуйста, введите счет отправителя:"))
     B = int(input("Пожалуйста, введите счет получателя:"))
     money = int(input('Пожалуйста, введите сумму перевода:'))
-    num = money
     try:
         cursor.execute('select money from bnk where idd={}'.format(A))
         a = int(cursor.fetchone()[0])
@@ -55,38 +55,39 @@ def transfer():     #перовод
         currency_a = cursor.execute('select currency from bnk where idd={}'.format(A)).fetchone()[0]
         currency_b = cursor.execute('select currency from bnk where idd={}'.format(B)).fetchone()[0]
 
-        if a < num:
+        if a < money:
             print("Перевод не выполнен, недостаточно остатка")
         else:
             if currency_b == currency_a:
-                # таблица с историей изменения баланса
-                val_1 = (A, currency_a, a - num)
+                # таблица с историей изменения баланса  A  tr2
+                val_1 = (A, currency_a, a - money)
                 sql_1 = "insert into tr2 values(to_number(:1),:2, to_number(:3))"
                 cursor.execute(sql_1, tuple(val_1))
 
-                # все транзакции по id A
-                val_1_1 = (A, currency_a, -num)
+                # все транзакции по id A     tr3
+
+                val_1_1 = (A, currency_a, -money)
                 sql_1_1 = "insert into tr3 values(to_number(:1),:2, to_number(:3))"
                 cursor.execute(sql_1_1, tuple(val_1_1))
 
-                # таблица с историей изменения баланса
-                val_2 = (B, currency_b, b + num)
+                # таблица с историей изменения баланса  B  tr2
+                val_2 = (B, currency_b, b + money)
                 sql_2 = "insert into tr2 values(to_number(:1), :2, to_number(:3))"
                 cursor.execute(sql_2, tuple(val_2))
 
-                # все транзакции по id B
-                val_2_1 = (B, currency_b, num)
+                # все транзакции по id B   tr3
+                val_2_1 = (B, currency_b, money)
                 sql_2_1 = "insert into tr3 values(to_number(:1), :2, to_number(:3))"
                 cursor.execute(sql_2_1, tuple(val_2_1))
 
-                cursor.execute('update bnk set money={} where idd={}'.format(a - num, A))
-                cursor.execute('update bnk set money={} where idd={}'.format(b + num, B))
+                cursor.execute('update bnk set money={} where idd={}'.format(a - money, A))
+                cursor.execute('update bnk set money={} where idd={}'.format(b + money, B))
                 conn.commit()  # change db
                 print("Передача прошла успешно!")
             else:
                 print("Деньги не переведено, разные валюты")
     except Exception:
-        print("Перевод не выполнен, и транзакция была отменена. Проверьте айди счета!")
+        print("Перевод не выполнен, и транзакция была отменена. Проверьте ID аккаунта!")
         conn.rollback()
 
 
@@ -97,23 +98,27 @@ def refill(): #ПОПОЛНЕНИЕ СЧЕТА
     input_idd = int(input("Пожалуйста, введите аккаунт айди для пополнение:"))
     new_moneyy = int(input("Пожалуйста, введите сумму:"))
     old_moneyy = cursor.execute('select money from bnk where idd={}'.format(input_idd)).fetchone()[0]
-    cursor = conn.cursor()
+    tr2_money = new_moneyy + old_moneyy
     cursor.execute('update bnk set money={} where idd={}'.format(old_moneyy + new_moneyy, input_idd))
+
+    currency_tr2 = cursor.execute('select currency from bnk where idd={}'.format(input_idd)).fetchone()[0]
+    val_tr2 = (input_idd, currency_tr2, tr2_money)
+    sql_tr2 = "insert into tr2 values(to_number(:1),:2, to_number(:3))"
+    cursor.execute(sql_tr2, tuple(val_tr2))
     conn.commit()
     print("Аккаунт по ID номер {} пополнено на сумму {}. Доступно:{}".format(input_idd, new_moneyy,
                                                                              old_moneyy + new_moneyy))
 
 def graph(): #ГРАФИК СЧЕТА
-    A_graph = int(input("Пожалуйста, введите аккаунт айди для просмотра истории баланса:"))
-    query = "SELECT * FROM tr2 where idd={}".format(A_graph)
-    otchet = pd.read_sql(query, con=conn)
-    x = otchet['MONEY']
- 
-    plt.title('График историей изменения баланса по ID - {} '.format(A_graph))
-    plt.ylabel('{}'.format(cursor.execute('select currency from bnk where idd={}'.format(A_graph)).fetchone()[0]))
+    a_graph = int(input("Пожалуйста, введите аккаунт айди для просмотра истории баланса:"))
+    query = "SELECT * FROM tr2 where idd={}".format(a_graph)
+    graphh = pd.read_sql(query, con=conn)
+    x = graphh['MONEY']
+
+    plt.title('График историей изменения баланса по ID - {} '.format(a_graph))
+    plt.ylabel('{}'.format(cursor.execute('select currency from bnk where idd={}'.format(a_graph)).fetchone()[0]))
 
     plt.plot(x)
     plt.show()
-
 
 
