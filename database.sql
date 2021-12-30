e1d93471fae2c1bfdfb0fcf6df9749bbd98ba54b
Khamitz <90205644+Khamitz@users.noreply.github.com>
2021-12-30 18:41:08 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,17 +1,21 @@
-у нас должен быть 3 таблицы: bnk, tr2, tr3.
+/* У нас должен быть 3 таблицы: 
+bnk - аккаунт
+tr2 - таблица с историей изменения баланса
+tr3 - транзакция каждого аккаунта */
+
 
 CREATE TABLE bnk (
     idd int NOT NULL PRIMARY KEY,
     currency varchar(255) NOT NULL,
     money int NOT NULL
-);  --аккаунт
+);
 
 
 CREATE TABLE tr2 (
     idd int NOT NULL,
     currency varchar(255) NOT NULL,
     money int NOT NULL
-);       -- таблица с историей изменения баланса. 
+);
 
 
 
@@ -19,11 +23,13 @@ CREATE TABLE tr3 (
     idd int NOT NULL,
     currency varchar(255) NOT NULL,
     money int NOT NULL
-);    --транзакция каждого аккаунта
+);
 
 
-select * FROM bnk; --аккаунт
 
-select * FROM tr2;  -- таблица с историей изменения баланса. 
 
-select * FROM tr3; --транзакция каждого аккаунта
+select * FROM bnk;
+
+select * FROM tr2;
+
+select * FROM tr3;
